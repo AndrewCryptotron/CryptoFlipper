cae14229d9299af39389ce06cfc5de95f2ddad12
Andrey Novikov <andrewnovikoff@outlook.com>
2022-12-27 06:52:44 +0300

    Default

---
+++ contracts/CryptoFlipper.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "hardhat/console.sol";
+import "./CryptoFlipperTicket.sol";
+
+interface CryptoTicketInterface {
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function sold() external view returns (uint256 ammount);
+}
+
+error CryptoFlipper__OwnerFailure();
+error CryptoFlipper__TransferFailure();
+error CryptoFlipper__OwnerRightsFailure();
+error CryptoFlipper__DetectedFailure();
+error CryptoFlipper__ZeroingFailure();
+
+contract CryptoFlipper is VRFConsumerBaseV2 {
+
+    enum cryptoFlipState {
+        OPEN,
+        CALCULATING
+    }
+
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    cryptoFlipState private s_cryptoFlipState;
+    bytes32 private immutable i_gasLane;
+    uint256 private s_lastTimeStamp;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 4;
+    address payable private owner;
+    address private currentContract;
+    address payable private headsPlayer;
+    address payable private tailsPlayer;
+    address payable private winner;
+    address private immutable nullAddress = address(0x0);
+    address private s_winner;
+    address[] public s_allWinners;
+    address[] public s_funders;
+    address[] private deprecatedContracts;
+    bool private failure = false;
+
+    event HeadsEnter(address indexed headsPlayer);
+    event TailsEnter(address indexed tailsPlayer);
+    event WinnerPicked(address indexed winner);
+    event RequestedCryptoFlipWinner(uint256 indexed requestId);
+    event AddressChanged(address indexed newAddress);
+    event NewFunder(address indexed funder);
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert CryptoFlipper__OwnerRightsFailure();
+        }
+        _;
+    }
+
+    modifier checkFailure() {
+        if (failure != false) {
+            revert CryptoFlipper__DetectedFailure();
+        }
+        _;
+    }
+
+    modifier contractRestriction() {
+        if (currentContract != nullAddress) {
+            revert CryptoFlipper__ZeroingFailure();
+        }
+        _;
+    }
+
+    constructor(
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_cryptoFlipState = cryptoFlipState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        owner = payable(msg.sender);
+        currentContract = nullAddress;
+    }
+
+    function changeAddress(address newAddress) public onlyOwner checkFailure contractRestriction {
+        currentContract = newAddress;
+        emit AddressChanged(newAddress);
+    }
+
+    function fundCryptotron() public payable checkFailure {
+        s_funders.push(payable(msg.sender));
+        emit NewFunder(msg.sender);
+    }
+
+    function enterFlipper(uint256 tokenId) public {
+        CryptoTicketInterface cti = CryptoTicketInterface(currentContract);
+        if (cti.ownerOf(tokenId) != msg.sender) {
+            revert CryptoFlipper__OwnerRightsFailure();
+        } else if (tokenId == 0) {
+            headsPlayer = payable(msg.sender);
+            emit HeadsEnter(msg.sender);
+        } else if (tokenId % 2 == 0) {
+            headsPlayer = payable(msg.sender);
+            emit HeadsEnter(msg.sender);
+        } else {
+            tailsPlayer = payable(msg.sender);
+            emit TailsEnter(msg.sender);
+        }
+        flip();
+    }
+
+    function flip() internal {
+        s_cryptoFlipState = cryptoFlipState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedCryptoFlipWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256, 
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 headsOrTails = randomWords[0] % 2;
+        if (headsOrTails == 0) {
+            winner = headsPlayer;
+        } else {
+            winner = tailsPlayer;
+        }
+        s_winner = winner;
+        s_allWinners.push(winner);
+        deprecatedContracts.push(currentContract);
+        currentContract = nullAddress;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            failure = true;
+            revert CryptoFlipper__TransferFailure();
+        }
+        s_lastTimeStamp = block.timestamp;
+        s_cryptoFlipState = cryptoFlipState.OPEN;
+        emit WinnerPicked(winner);
+    }
+
+}
